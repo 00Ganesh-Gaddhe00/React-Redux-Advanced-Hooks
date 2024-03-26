53e6f9c9eabb51b5658283311f7a47a0c5b7eaa8
Ganesh <00mr.lg00@gmail.com>
2024-03-26 18:43:25 +0530

    useReducers

--- adv-hooks/src/App.js
+++ adv-hooks/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import List from './components/list';
-
+import Todo from './components/Todo';
+import TodoReducer from './components/TodoReducer';
 function App() {
   return (
     <>
-    <List/>
+      <TodoReducer/>
+    {/* <List/> */}
     </>
   );
 }
